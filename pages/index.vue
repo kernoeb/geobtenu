0aa16f401cd65dc411d22351c9c146aba4000838
kernoeb <kernoeb@protonmail.com>
2021-01-16 21:58:54 +0100

    fix: xl, search mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,45 @@
 <template>
-  <v-row
-    align="center"
-    justify="center"
-    style="justify-content: center"
-  >
-    <v-col v-for="value in countries" :key="`flag_${value.id}`" cols="5" md="3" xl="2">
-      <nuxt-link :to="{name: 'flag-slug', params: {slug: value.id}}" style="text-decoration: none;">
-        <v-card>
-          <v-img
-            :src="require(`../assets/flags/png/${value.id}.png`)"
-            class="flag"
-            gradient="to top left, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-            height="180"
-          >
-            <template #placeholder>
-              <v-skeleton-loader />
-            </template>
-            <v-card-title style="color: white">
-              {{ value.country.split('|')[0] }}
-            </v-card-title>
-            <v-card-subtitle v-if="value.capital" style="color: white">
-              {{ value.capital }}
-            </v-card-subtitle>
-          </v-img>
-        </v-card>
-      </nuxt-link>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        lg="8"
+        offset-lg="2"
+      >
+        <v-row>
+          <v-text-field v-model="search" placeholder="Rechercher un pays ou une capitale" />
+        </v-row>
+        <v-row
+          align="center"
+          justify="center"
+          style="justify-content: center"
+        >
+          <v-col v-for="value in countriesFiltered" :key="`flag_${value.id}`" cols="5" md="3" xl="3">
+            <nuxt-link :to="{name: 'flag-slug', params: {slug: value.id}}" style="text-decoration: none;">
+              <v-card>
+                <v-img
+                  :src="require(`../assets/flags/png/${value.id}.png`)"
+                  class="flag"
+                  gradient="to top left, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+                  height="180"
+                >
+                  <template #placeholder>
+                    <v-skeleton-loader />
+                  </template>
+                  <v-card-title style="color: white">
+                    {{ value.country.split('|')[0] }}
+                  </v-card-title>
+                  <v-card-subtitle v-if="value.capital" style="color: white">
+                    {{ value.capital }}
+                  </v-card-subtitle>
+                </v-img>
+              </v-card>
+            </nuxt-link>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -37,7 +50,19 @@ export default {
   data () {
     return {
       countries,
-      mounted: false
+      mounted: false,
+      search: ''
+    }
+  },
+  computed: {
+    countriesFiltered () {
+      if (!this.search.length) {
+        return this.countries
+      } else {
+        return this.countries.filter(country =>
+          country.country.toUpperCase().includes(this.search.toUpperCase()) ||
+          (country.capital && country.capital.toUpperCase().includes(this.search.toUpperCase())))
+      }
     }
   },
   mounted () {
