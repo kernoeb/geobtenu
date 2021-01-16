5f6bf5b869546e11eacdc7333f2162c37879aeb7
kernoeb <kernoeb@protonmail.com>
2021-01-16 22:40:25 +0100

    Revert "Update nuxt.config.js"
    This reverts commit 2e4ff8da486a31f80c7d48dc4b50791a8909999f.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const isDev = NODE_ENV === 'development'
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
