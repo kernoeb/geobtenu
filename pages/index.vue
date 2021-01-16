398865c7b331e0edb4c959212df7dc78a352bbc3
kernoeb <kernoeb@protonmail.com>
2021-01-16 19:26:44 +0100

    fix: lint

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             gradient="to top left, rgba(100,115,201,.33), rgba(25,32,72,.7)"
             height="180"
           >
-            <template v-slot:placeholder>
+            <template #placeholder>
               <v-skeleton-loader />
             </template>
             <v-card-title style="color: white">
