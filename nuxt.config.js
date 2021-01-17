61dd4e92091df5ac13f059547cd455154a8de111
kernoeb <kernoeb@protonmail.com>
2021-01-17 21:09:16 +0100

    add pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxtjs/pwa'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
