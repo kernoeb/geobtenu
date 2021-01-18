a5175def797111835d241ca408bbc66989f8dfc1
kernoeb <kernoeb@protonmail.com>
2021-01-18 18:00:29 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Un outil simple et efficace pour obtenir des informations sur un pays !' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
