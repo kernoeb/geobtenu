814cd777c1f564b0f33f9f3771d344267e6f1cfe
kernoeb <kernoeb@protonmail.com>
2021-01-25 13:06:54 +0100

    Update credits.vue

--- pages/credits.vue
+++ pages/credits.vue
@@ -10,7 +10,18 @@
           align="center"
           justify="center"
         >
-          Merci à&nbsp;<a href="https://www.twitch.tv/armire37">Armire</a>,&nbsp;<a href="https://www.twitch.tv/letakamari">Takamari</a>,&nbsp;<a href="https://www.twitch.tv/jussetain">Jussetain</a>, Google et Wikipédia
+          Merci à&nbsp;<a target="_blank" href="https://www.twitch.tv/armire37">Armire</a>,&nbsp;<a target="_blank" href="https://www.twitch.tv/letakamari">Takamari</a>,&nbsp;<a target="_blank" href="https://www.twitch.tv/jussetain">Jussetain</a>, Google et Wikipédia
+        </v-row>
+        <v-row align="center" justify="center" class="title mb-3">
+          Réalisé par&nbsp;<a target="_blank" href="https://www.twitter.com/kernoeb">@kernoeb</a>
+        </v-row>
+        <v-row align="center" justify="center" class="d-flex justify-center flex-column">
+          <div>
+            Ce site est <i>open-source</i> ! Le projet est sur&nbsp;<a target="_blank" href="https://github.com/kernoeb/geobtenu">GitHub</a>.
+          </div>
+          <div>
+            N'hésitez pas à faire une petite <i>pull request</i> pour participer au projet :)
+          </div>
         </v-row>
       </v-col>
     </v-row>
