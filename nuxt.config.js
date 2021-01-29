76f4a05b0647a0888b284e3130a7bd2e8817810f
kernoeb <kernoeb@protonmail.com>
2021-01-29 23:44:35 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,8 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content'
+    '@nuxt/content',
+    'vue-plausible'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
